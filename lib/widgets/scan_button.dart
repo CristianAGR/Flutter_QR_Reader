58ce704e59396beef7e53d0ac9407e71d20d7c19
CristianAGR <cgr72677@udelasalle.edu.mx>
2022-12-16 15:27:28 -0600

    Se agrego el scan de qr

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanButton extends StatelessWidget {
   const ScanButton({super.key});
@@ -6,8 +7,10 @@ class ScanButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      onPressed: (() {
-        
+      onPressed: (() async {
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#3D8BEF', 'Cancelar', false, ScanMode.QR);
+
+        print(barcodeScanRes);
       }),
       elevation: 0,
       child: const Icon(Icons.filter_center_focus),
