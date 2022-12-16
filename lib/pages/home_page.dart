180d3b3198aa1310cf7f7fe26ffdeffe1479b439
CristianAGR <cgr72677@udelasalle.edu.mx>
2022-12-15 23:27:02 -0600

    Se implemento un gestor de estado en HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:qr_reader/pages/direcciones_page.dart';
+import 'package:qr_reader/pages/mapas_page.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
 import 'package:qr_reader/widgets/custom_navigatorbar.dart';
 import 'package:qr_reader/widgets/scan_button.dart';
 
@@ -20,12 +25,35 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
-      body: const Center(
-         child: Text('HomePage'),
+      body: _HomePageBody(
+
       ),
       bottomNavigationBar: const CustomNavigationBar(),
       floatingActionButton: const ScanButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+  }
+}
+
+class _HomePageBody extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    // Obtener el selected menu opt
+    final uiProvider = Provider.of<UiProvider>(context);
+    // cambiar para mostrar la pagina respectiva
+    final selectedIndex = uiProvider.selectedMenuOpt;
+
+    switch( selectedIndex ) {
+      case 0:
+      return const MapasPage();
+
+      case 1:
+      return const DireccionesPage();
+
+      default:
+      return const MapasPage();
+    }
   }
 }
