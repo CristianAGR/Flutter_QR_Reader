e3f2034a598b5269357e10e11c16e629eda504a7
CristianAGR <cgr72677@udelasalle.edu.mx>
2022-12-15 19:15:26 -0600

    First Commit!

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:qr_reader/widgets/custom_navigatorbar.dart';
+import 'package:qr_reader/widgets/scan_button.dart';
+
+class HomePage extends StatelessWidget {
+   
+  const HomePage({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text('Historial'),
+        actions: [
+          IconButton(onPressed: () {
+            
+          },
+          icon: const Icon(Icons.delete_forever)
+          )
+        ],
+      ),
+      body: const Center(
+         child: Text('HomePage'),
+      ),
+      bottomNavigationBar: const CustomNavigationBar(),
+      floatingActionButton: const ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
