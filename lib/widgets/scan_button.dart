3338e5fd7f173dc984646b50d6024102e9f675aa
CristianAGR <cgr72677@udelasalle.edu.mx>
2022-12-21 20:27:20 -0600

    Se agregan nuevos scans con qrButton

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/providers/scan_list_provider.dart';
 
 class ScanButton extends StatelessWidget {
   const ScanButton({super.key});
@@ -8,9 +10,14 @@ class ScanButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return FloatingActionButton(
       onPressed: (() async {
-        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#3D8BEF', 'Cancelar', false, ScanMode.QR);
+        //String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#3D8BEF', 'Cancelar', false, ScanMode.QR);
+        final barcodeScanRes = 'https://fernando-herrera.com';
 
-        print(barcodeScanRes);
+        // busca en el arbol de widgets la instancia de ScanListProvider
+        final scanListProvider = Provider.of<ScanListProvider>(context, listen: false);
+        scanListProvider.nuevoScan(barcodeScanRes);
+        scanListProvider.nuevoScan('geo:15.33,15.66');
+        //print(barcodeScanRes);
       }),
       elevation: 0,
       child: const Icon(Icons.filter_center_focus),
