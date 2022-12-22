b184ffe5bb361b051043962b09ca537c2b0c40b7
CristianAGR <cgr72677@udelasalle.edu.mx>
2022-12-21 20:26:55 -0600

    CRUD de base de datos

--- lib/providers/db_provider.dart
+++ lib/providers/db_provider.dart
@@ -5,6 +5,9 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 
+import 'package:qr_reader/models/scan_model.dart';
+export 'package:qr_reader/models/scan_model.dart';
+
 class DBProvider {
 
   static Database? _database;
@@ -25,7 +28,6 @@ class DBProvider {
     // Path de donde almacenaremos la base de datos
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
     final path = join( documentsDirectory.path, 'ScansDB.db' );
-    print( path );
 
     // Crear base de datos
     return await openDatabase(
@@ -45,4 +47,83 @@ class DBProvider {
     );
 
   }
+
+  Future<int>nuevoScanRaw(ScanModel nuevoScan) async {
+
+     final id = nuevoScan.id;
+     final tipo = nuevoScan.tipo;
+     final valor = nuevoScan.valor;
+     
+     // verificar la base de datos
+     final db = await database;
+
+     final res = await db!.rawInsert('''
+     INSERT INTO Scans ( id, tipo, valor )
+     VALUES ( $id,'$tipo','$valor' )
+     ''');
+
+     return res;
+  }
+
+  Future<int>nuevoScan(ScanModel nuevoScan) async {
+    final db = await database;
+    final res = await db!.insert('Scans', nuevoScan.toJson());
+    // Es el ID del último registro insertado
+    return res;
+  }
+
+  Future<ScanModel?> getScanById( int id ) async {
+
+    final db  = await database;
+    final res = await db!.query('Scans', where: 'id = ?', whereArgs: [id]);
+
+    return res.isNotEmpty
+          ? ScanModel.fromJson( res.first )
+          : null;
+  }
+
+  Future<List<ScanModel>?> getScans() async{
+
+    final db = await database;
+    final res = await db!.query('Scans');
+
+    return res.isNotEmpty
+    ? res.map( (s) => ScanModel.fromJson(s) ).toList()
+    : [];
+  }
+
+  Future<List<ScanModel>?> getScansPorTipo(String tipo) async{
+
+    final db = await database;
+    final res = await db!.rawQuery('''
+      SELECT * FROM Scans WHERE tipo = '$tipo'    
+    ''');
+
+    // guarda los resultados en un mapa y lo convierte en una lista
+    return res.isNotEmpty
+    ? res.map( (s) => ScanModel.fromJson(s) ).toList()
+    : [];
+  }
+
+
+  Future<int> updateScan( ScanModel nuevoScan ) async {
+    final db  = await database;
+    final res = await db!.update('Scans', nuevoScan.toJson(), where: 'id = ?', whereArgs: [ nuevoScan.id ] );
+    return res;
+  }
+
+  Future<int> deleteScan( int id ) async {
+    final db  = await database;
+    final res = await db!.delete( 'Scans', where: 'id = ?', whereArgs: [id] );
+    return res;
+  }
+
+  Future<int> deleteAllScans() async {
+    final db  = await database;
+    final res = await db!.rawDelete('''
+      DELETE FROM Scans    
+    ''');
+    return res;
+  }
+
 }
