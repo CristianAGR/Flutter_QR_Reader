0fd7c98fb2c918e81a7b1742c9ab9d810608a84d
CristianAGR <cgr72677@udelasalle.edu.mx>
2022-12-23 14:58:48 -0600

    funcion para retornar latitud y longitud

--- lib/models/scan_model.dart
+++ lib/models/scan_model.dart
@@ -1,6 +1,8 @@
 
 import 'dart:convert';
 
+import 'package:google_maps_flutter/google_maps_flutter.dart' show LatLng;
+
 ScanModel scanModelFromJson(String str) => ScanModel.fromJson(json.decode(str));
 
 String scanModelToJson(ScanModel data) => json.encode(data.toJson());
@@ -21,6 +23,16 @@ class ScanModel {
     int? id;
     String? tipo;
     String valor;
+
+    // obtener geolocalizacion
+    LatLng getLatLng() {
+
+      final latLng = valor.substring(4).split(',');
+      final lat = double.parse(latLng[0]);
+      final lng = double.parse(latLng[1]);
+
+      return LatLng(lat, lng);
+    }
 
     // regresa una nueva instancia de la clase
     factory ScanModel.fromJson(Map<String, dynamic> json) => ScanModel(
