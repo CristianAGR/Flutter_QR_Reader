c3722433505748888301e44cc92ef1b59cd55ce2
CristianAGR <cgr72677@udelasalle.edu.mx>
2022-12-23 17:29:43 -0600

    Se escanea el qr y se lanza el url

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:provider/provider.dart';
 import 'package:qr_reader/providers/scan_list_provider.dart';
+import 'package:qr_reader/utils/utils.dart';
 
 class ScanButton extends StatelessWidget {
   const ScanButton({super.key});
@@ -10,14 +11,22 @@ class ScanButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return FloatingActionButton(
       onPressed: (() async {
-        //String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#3D8BEF', 'Cancelar', false, ScanMode.QR);
-        final barcodeScanRes = 'https://fernando-herrera.com';
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#3D8BEF', 'Cancelar', false, ScanMode.QR);
+        //const barcodeScanRes = 'https://fernando-herrera.com';
+        //const barcodeScanRes = 'geo:45.287135,-75.920226';
+
+        // Si el usuario cancela no se hace nada
+        if ( barcodeScanRes == '-1' ) {
+          return;
+        }
 
         // busca en el arbol de widgets la instancia de ScanListProvider
+        // ignore: use_build_context_synchronously
         final scanListProvider = Provider.of<ScanListProvider>(context, listen: false);
-        scanListProvider.nuevoScan(barcodeScanRes);
-        scanListProvider.nuevoScan('geo:15.33,15.66');
-        //print(barcodeScanRes);
+        final nuevoScan = await scanListProvider.nuevoScan(barcodeScanRes);
+        
+        // ignore: use_build_context_synchronously
+        launchURL(context, nuevoScan);
       }),
       elevation: 0,
       child: const Icon(Icons.filter_center_focus),
