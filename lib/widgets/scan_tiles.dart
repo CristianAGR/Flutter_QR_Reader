1f254e317f40cef357da777cd8ff39d998968586
CristianAGR <cgr72677@udelasalle.edu.mx>
2022-12-22 22:58:29 -0600

    Se implemento la funcion launchURL

--- lib/widgets/scan_tiles.dart
+++ lib/widgets/scan_tiles.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:qr_reader/utils/utils.dart';
 import '../providers/scan_list_provider.dart';
 
 class ScanTiles extends StatelessWidget {
@@ -33,7 +34,7 @@ class ScanTiles extends StatelessWidget {
           title: Text(scans[i].valor),
           subtitle: Text(scans[i].id.toString()),
           trailing: const Icon(Icons.keyboard_arrow_right, color:  Colors.grey,),
-          onTap: () => print(scans[i].id.toString()),
+          onTap: () => launchURL(context, scans[i]),
         ),
       ),
     );
